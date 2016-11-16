6eafcb97eb9717b1a6f40c4f8ff0494713ca0832
Ion D. FIlho <iondrimba@hotmail.com>
2016-11-16 18:21:34 -0200

    Fix default number of itens all time winners

--- MusicBattlWebAPI/Scripts/collections/allTimeWinnersCollection.js
+++ MusicBattlWebAPI/Scripts/collections/allTimeWinnersCollection.js
@@ -4,6 +4,7 @@
         { Picture: "dummy-artist.jpg", AlbumCover: "dummy-album.jpg", ArtistName: "", SongName: "", AlbumName: "", ArtistId: 0 },
         { Picture: "dummy-artist.jpg", AlbumCover: "dummy-album.jpg", ArtistName: "", SongName: "", AlbumName: "", ArtistId: 0 },
         { Picture: "dummy-artist.jpg", AlbumCover: "dummy-album.jpg", ArtistName: "", SongName: "", AlbumName: "", ArtistId: 0 },
+        { Picture: "dummy-artist.jpg", AlbumCover: "dummy-album.jpg", ArtistName: "", SongName: "", AlbumName: "", ArtistId: 0 },
         { Picture: "dummy-artist.jpg", AlbumCover: "dummy-album.jpg", ArtistName: "", SongName: "", AlbumName: "", ArtistId: 0 }
     ], {
         model: Model
